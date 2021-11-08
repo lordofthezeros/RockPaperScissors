d787cd24432f0a4b0cf9ed78ad951718d3dcb043
lordofthezeros <cee.pritchard@gmail.com>
2021-11-08 09:30:56 -0500

    Add playRound function to play one game of Rock Paper Scissors

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
-let choices = ["rock", "paper", "scissors"]
-
 function computerPlay() {
     return choices[Math.floor(choices.length * Math.random())];
 
 }
 
-let computerChoice = computerPlay()
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw, play again!";
+    }
+    else if ((playerSelection === 'rock' && computerSelection === 'paper') ||  
+            (playerSelection === 'paper' && computerSelection === 'scissors') || 
+            (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    
+}
 
-let playerChoice = prompt
+let choices = ["rock", "paper", "scissors"]
+
+let computerChoice = computerPlay().toLowerCase()
+
+let playerChoice = prompt("Make your choice! Type Rock, Paper, or Scissors to choose").toLowerCase()
+
+console.log(playRound(playerChoice, computerChoice))
